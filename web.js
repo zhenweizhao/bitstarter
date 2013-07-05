4737bda1de0969f794163a1f1fbe430d6a3f9c00
Zhenwei Zhao <zzhao75@gmail.com>
2013-07-05 04:33:35 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs')
+var buffer = Buffer.toString(fs.readFile('index.html'));
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
