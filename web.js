4486e7d9faccb09979366b424d29108e7ca63150
Zhenwei Zhao <zzhao75@gmail.com>
2013-08-06 03:10:36 +0000

    final clean up for hw6

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var fs = require('fs')
-var buffer = fs.readFileSync('index.html').toString();
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer);
+    var buffer = fs.readFileSync('index.html').toString();
+    response.send(buffer);
 });
 
 var port = process.env.PORT || 8080;
